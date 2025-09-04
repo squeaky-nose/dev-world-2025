395628455a1408df3c3bbe6651249ba84154fc0c
Sushant Verma <sushant.40@gmail.com>
2025-09-04 23:22:16 +1000

    Presentation demos

--- ImagesApp/ImagesApp/DependencyInjection/DependencyResolver+Previews.swift
+++ ImagesApp/ImagesApp/DependencyInjection/DependencyResolver+Previews.swift
@@ -15,7 +15,21 @@ extension DependencyResolver {
             PreviewImageLoader()
         }
         resolver.register(ImageFavouritesService.self, resolved: .singleton) { _ in
-            InMemoryImageFavourites()
+            let favourites = InMemoryImageFavourites()
+
+//            favourites.save(.init(id: 1076758,
+//                                  width: 3230,
+//                                  height: 3648,
+//                                  alt: "Close-up of a vibrant jellyfish gracefully swimming in the clear waters of Illes Balears, Spain.",
+//                                  photographer: "Pawel Kalisinski",
+//                                  src: PexelsPhoto.Src(
+//                                    original: "https://images.pexels.com/photos/1076758/pexels-photo-1076758.jpeg",
+//                                    large: "https://images.pexels.com/photos/1076758/pexels-photo-1076758.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+//                                    medium: "https://images.pexels.com/photos/1076758/pexels-photo-1076758.jpeg?auto=compress&cs=tinysrgb&h=350",
+//                                    small: "https://images.pexels.com/photos/1076758/pexels-photo-1076758.jpeg?auto=compress&cs=tinysrgb&h=130"
+//                                  )))
+
+            return favourites
         }
         resolver.register(ImageAnalysisService.self, resolved: .perResolution) { _ in
             PreviewImageAnalysisService()
